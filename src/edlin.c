2706ff73758643468c722bec9ac26378b2d759a4
Bill Chatfield <bill_chatfield@yahoo.com>
2021-12-27 12:57:51 -0500

    Initial check-in

---
+++ src/edlin.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : edlin.c
+ Author      : Bill Chatfield
+ Version     :
+ Copyright   : GPL3
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void openFile(const char *name);
+
+int main(int argc, const char *argv[])
+{
+	if (argc > 1) {
+		openFile(argv[1]);
+	}
+	return EXIT_SUCCESS;
+}
+
+void openFile(const char *name) {
+
+}
